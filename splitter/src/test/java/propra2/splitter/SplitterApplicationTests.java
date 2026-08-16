17d9fea351a2fdf3ea31f9dd65b8706c956aa698
ixYozora <itachimasoudian@gmail.com>
2026-08-16 12:44:25 +0200

    test: run the tests against a real Postgres instead of H2

--- splitter/src/test/java/propra2/splitter/SplitterApplicationTests.java
+++ splitter/src/test/java/propra2/splitter/SplitterApplicationTests.java
@@ -2,8 +2,10 @@ package propra2.splitter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersKonfiguration.class)
 class SplitterApplicationTests {
 
   @Test
